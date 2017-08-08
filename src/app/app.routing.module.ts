f163ee4bc3eefe8cd17d29d65be19f07a93fb2eb
Michael O'Keefe <m.okeefe@kunzleigh.com>
2017-08-08 19:29:44 -0400

    done! moving on to animations

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {Routes, RouterModule, PreloadAllModules} from '@angular/router';
 import {HomeComponent} from './core/home/home.component';
 import {AuthGuardService} from './service/auth-guard.service';
+import {RecipesModule} from './recipes/recipes.module';
+
 
 // dynamic params come after hard routes
 // loadChildren for lazy loading
 const appRoutes: Routes = [
   {path: '', component: HomeComponent },
-  {path: 'recipes', loadChildren: './recipes/recipe.module#RecipesModule', canLoad: [AuthGuardService] }
+  {path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule', canLoad: [AuthGuardService] }
 ];
 
+// preload all lazy loaded modules as the app is being used.
 @NgModule({
   imports: [
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes, {preloadingStrategy: PreloadAllModules})
   ],
   exports: [
     RouterModule
